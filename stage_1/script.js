d0be14df319b15483ef933cbb7df6b8bbe95aa74
Odunayo Dauda <odunayo.dauda@ubagroup.com>
2023-09-11 10:24:51 +0100

    First commit

---
+++ stage_1/script.js
@@ -0,0 +1,18 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDate = new Date();
+    const currentDay = daysOfWeek[currentDate.getUTCDay()];
+    const currentUTCTime = currentDate.getTime();
+    console.log(currentUTCTime)
+    console.log(currentDate)
+
+    // Update the elements with data-testid attributes
+    document.querySelector("[data-testid='currentDayOfTheWeek']").textContent = "Today is: " + currentDay;
+    document.querySelector("[data-testid='currentUTCTime']").textContent = "You have used " + currentUTCTime + " milliseconds today";
+
+    // Set your track and GitHub URL
+    document.querySelector("[data-testid='myTrack']").textContent = "Frontend";
+    const githubURL = "https://github.com/D-o-v/HNG-Internship"; // Replace with your GitHub URL
+    document.querySelector("[data-testid='githubURL']").href = githubURL;
+});
